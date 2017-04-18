c78777a7124f0fb738228e13653aa761126ec2a2
DESKTOP-QHNF8NB\toyo <toyo0103@gmail.com>
2017-04-18 16:27:36 +0800

    Commit project

---
+++ UnitTestDay1/EasyMethod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestDay1
+{
+    public class EasyMethod
+    {
+        private int BaseNumber = 2;
+
+        public int Method1()
+        {
+            return BaseNumber + 5;
+        }
+
+        public int Method2()
+        {
+            return BaseNumber + 8;
+        }
+    }
+}
---
+++ UnitTestDay1Tests/EasyMethodTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestDay1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestDay1.Tests
+{
+    [TestClass()]
+    public class EasyMethodTests
+    {
+        [TestMethod()]
+        public void Method1Test_呼叫時應回傳結果為7()
+        {
+            //assert
+            var Sut = new EasyMethod();
+            var expected = 7;
+
+            //arrange
+            var actual = Sut.Method1();
+
+            //act
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Method2Test_呼叫時應回傳結果為10()
+        {
+            //assert
+            var Sut = new EasyMethod();
+            var expected = 10;
+
+            //arrange
+            var actual = Sut.Method2();
+
+            //act
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
